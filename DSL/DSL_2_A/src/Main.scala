07e78e2da3cbc6d18adc836d6c45a9b743e72ed5
uncho <uncho.tnct@gmail.com>
2016-01-25 01:15:00 +0900

    Case 12 Runtime Error変わらず。入力は原因ではない？

--- DSL/DSL_2_A/src/Main.scala
+++ DSL/DSL_2_A/src/Main.scala
@@ -1,24 +1,24 @@
 import scala.io.StdIn.readLine
 
 object Main {
-	
+
 	def find(array: Seq[Int], s: Int, t: Int): Int = array.slice(s, t+1).min
-	
+
 	def update(array: Seq[Int], i: Int, x: Int): Seq[Int] = array.updated(i,x)
-	
+
 	def main(args: Array[String]){
 		val in = readLine().split(" ").map(_.toInt)
 		val n = in(0)
 		val q = in(1)
-		
+
 		var a = Seq.fill(n)(Integer.MAX_VALUE)
-		
+
 		for(_ <- 1 to q){
 			val in = readLine().split(" ").map(_.toInt)
 			val com = in(0)
 			val x = in(1)
 			val y = in(2)
-			
+
 			if(com==0) a = update(a,x,y)
 			else println(find(a,x,y))
 		}
